911a2c8c983668a6d61dcbfd58dd1c0f7f84bc55
Manish Tiwari <69393822+manish-9245@users.noreply.github.com>
2022-01-20 18:56:35 +0530

    Create treespreorder.c

---
+++ treespreorder.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node *root=NULL;
+void insert(int d)
+{
+    struct node *temp;
+    temp=root;
+    struct node *new;
+    new=(struct node*)malloc(sizeof(struct node));
+    new->data=d;
+    new->left=NULL;
+    new->right=NULL;
+    if(root==NULL)
+    {
+        root=new;
+    }
+    else
+    {
+        while(1)
+        {
+            if(d<(temp->data))
+            {
+                if(temp->left!=NULL)
+                {
+                    temp=temp->left;
+                }
+                else
+                {
+                    temp->left=new;
+                    break;
+                }
+            }
+            else
+            {
+                if(temp->right!=NULL)
+                {
+                    temp=temp->right;
+                }
+                else
+                {
+                    temp->right=new;
+                    break;
+                }
+            }
+            
+        }
+    }
+    
+}
+
+void preorder(struct node *temp)
+{
+    if(temp==NULL)
+    {
+        return;
+    }
+    else
+    {
+        
+        printf("%d ",temp->data);
+        preorder(temp->left);
+        preorder(temp->right);
+    }
+}
+
+int main()
+{
+    int d;
+    do{
+        scanf("%d",&d);
+        if(d>0)
+        {
+            insert(d);
+        }
+    }while(d>0);
+    printf("Preorder Traversal:\n");
+    preorder(root);
+    return 0;
+}
